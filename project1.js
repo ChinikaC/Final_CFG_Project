513b4b490cf15c20ae4404fe0cc8e542c183a519
Chinika07 <105714970+Chinika07@users.noreply.github.com>
2022-11-16 18:33:08 +0000

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,10 @@
+let question = "Want to subscribe to the mailing list to receive free receipes? Please provide your email address below";
+let answer = prompt(question);
+let welcomemessage = "Happy baking! :)"
+let otherMessage = ""; 
+
+if(answer == "email@cfg.com"){
+    alert(welcomemessage);
+} else {
+    alert(otherMessage);
+};
